3a5f53ea15bc8f16459f57200bb83c28a92f95b4
Tracy Falba <tfalba@mac.com>
2026-01-01 23:16:01 -0500

    fix x scroll

--- client/src/components/ManagePlaylistAndSearch.tsx
+++ client/src/components/ManagePlaylistAndSearch.tsx
@@ -426,7 +426,7 @@ export default function ManagePlaylistAndSearch({
               "flex h-fit min-h-0 flex-col rounded-[24px] border border-white/10 bg-white/5 p-2 lg:p-3 text-white shadow-glow"
             )}
           >
-            <div className="mb-1 flex flex-col md:flex-row items-start md:items-center justify-between md:justify-start gap-3 flex-wrap">
+            <div className="mb-1 flex flex-col md:flex-row items-start md:items-center justify-between md:justify-start gap-3 flex-wrap overflow-x-hidden">
               <div className="flex items-center">
                 <h2
                   className={clsx(
